04dabb7ee872d2efb7165bb8f92f92b555b7e59b
Yang <75043852+caumannerman@users.noreply.github.com>
2022-04-08 17:01:03 +0900

    Add annotation on Application file

--- src/main/java/com/example/firstspringbootproject/FirstspringbootprojectApplication.java
+++ src/main/java/com/example/firstspringbootproject/FirstspringbootprojectApplication.java
@@ -3,9 +3,10 @@ package com.example.firstspringbootproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링부트 자동설정,
 public class FirstspringbootprojectApplication {
 
+	//SpringApplication.run으로 인해 스프링부트가 내장 WAS( Web App Server)를 실행함
 	public static void main(String[] args) {
 		SpringApplication.run(FirstspringbootprojectApplication.class, args);
 	}
